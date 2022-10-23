ccaa318353f6785b38707a645e28efdff77769f1
muhammadkhanusmanov <uqaxromon@gmail.com>
2022-10-23 17:16:10 +0500

    5-masala

--- Index05.py
+++ Index05.py
@@ -6,4 +6,17 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    u = s.upper()
+    l = s.lower()
+    i = 0
+    if u[0]==l[0]:
+        i+=1
+    if u[1]==l[1]:
+        i+=1
+    if u[2]==l[2]:
+        i+=1
+    if u[3]==l[3]:
+        i+=1
+    if u[4]==l[4]:
+        i+=1
+    return i 
